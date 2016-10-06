9c10aadc4817e7852f4eca460f7acaf93d114375
gulci-poz <sebastian.gulczynski.poznan@gmail.com>
2016-10-06 14:39:59 +0200

    2016-10-06 minor updates

--- src/main/java/com/gulci/spring/spring_yeah/MainSpring.java
+++ src/main/java/com/gulci/spring/spring_yeah/MainSpring.java
@@ -1,7 +1,9 @@
 package com.gulci.spring.spring_yeah;
 
 import com.gulci.spring.spring_yeah.api.UsersRepository;
+import com.gulci.spring.spring_yeah.domain.User;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainSpring {
@@ -9,6 +11,8 @@ public class MainSpring {
         ApplicationContext context = new ClassPathXmlApplicationContext("configuration.xml");
 
         UsersRepository usersRepository = context.getBean("myUsersRepository", UsersRepository.class);
-        usersRepository.createUser("gulci");
+        User user = usersRepository.createUser("gulci");
+        System.out.println(user);
+        ((ConfigurableApplicationContext) context).close();
     }
 }
